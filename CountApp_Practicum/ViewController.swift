c0ae67c50af1f86c33309daf5ff9288e87bd6eb8
maryvasileva <marysolodova1998@gmail.com>
2022-10-27 13:19:15 +0300

    Initial Commit

---
+++ CountApp_Practicum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountApp_Practicum
+//
+//  Created by Мария Солодова on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
