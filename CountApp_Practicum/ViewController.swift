09286c1bf1d74a15b81c8136330d49bac894fc12
maryvasileva <marysolodova1998@gmail.com>
2022-10-27 13:52:45 +0300

    Создана реализация лейбла и кнопки для приложения-счетчика.

--- CountApp_Practicum/ViewController.swift
+++ CountApp_Practicum/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tapsCount = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        
+        tapsCount += 1
+        updateLabelText()
+    }
+    
+    func updateLabelText() {
+        countLabel.text = "Значение счетчика: \(tapsCount)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        countLabel.textColor = .white
+        buttonLabel.tintColor = .white
+        updateLabelText()
     }
 
 
